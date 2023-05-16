096d266ecaaba362b31258ad9c3ba917a0154bfd
maximbelo <mb@tineye.com>
2023-05-16 15:30:26 -0400

    Displaying the selected square and the click history in the sidebar

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,9 +1,19 @@
 <template>
   <aside class="sidebar">
-    <h2>Clicked Squares</h2>
+    <h2>Selected square:</h2>
+    <p>{{ store.selectedSquare.file }}{{ store.selectedSquare.rank }}</p>
+
+    <h2>Selected history:</h2>
+    <ul>
+      <li v-for="item in store.history" :key="item">{{ item.file }}{{ item.rank }}</li>
+    </ul>
   </aside>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useSelectedSquareStore } from "@/stores/selectedSquare";
+
+const store = useSelectedSquareStore();
+</script>
 
 <style lang="scss" scoped></style>
