20967b8fb0d4e86e6ab507e47b0eeaaf07da7520
maximbelo <mbelomestnov@hotmail.ca>
2023-05-16 00:20:34 -0400

    Added some comments to the logic in the Pinia store and the ChessBoard comp

--- src/components/ChessBoard.vue
+++ src/components/ChessBoard.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="chessboard">
+    <!-- Render ranks (rows) -->
     <div v-for="rank in ranks" :key="rank" class="rank">
+      <!-- Render files (cols) inside the ranks -->
       <div
         v-for="file in files"
         :key="file"
@@ -9,6 +11,7 @@
       ></div>
     </div>
   </div>
+  <!-- ./chessboard -->
 </template>
 
 <script setup lang="ts">
@@ -20,11 +23,15 @@ const files = ["a", "b", "c", "d", "e", "f", "g", "h"];
 const ranks = ["1", "2", "3", "4", "5", "6", "7", "8"].reverse();
 
 const selectSquare = (file: string, rank: string): void => {
+  // When a user clicks on the square, this calls the selectSquare action in the Pinia store to update the selected square state.
   store.selectSquare(file, rank);
 };
 
 const getSquareClass = (file: string, rank: string): string => {
+  // Determine if the square is a light square based on its position on the board.
   const isLightSquare = (files.indexOf(file) + ranks.indexOf(rank)) % 2 === 0;
+
+  // Return the appropriate class based on whether it's a light or dark square.
   return isLightSquare ? "square dark" : "square light";
 };
 </script>
--- src/stores/selectedSquare.ts
+++ src/stores/selectedSquare.ts
@@ -3,16 +3,21 @@ import type { SelectedSquare, HistoryItem } from "@/types";
 
 export const useSelectedSquareStore = defineStore("selectedSquare", {
   state: () => ({
+    // Initialize the selectedSquare state with null values for file and rank
     selectedSquare: {
       file: null,
       rank: null,
     } as SelectedSquare,
+    // Initialize the history state as an empty array
     history: [] as HistoryItem[],
   }),
+
   actions: {
+    // This is responsible for updating the selected square and the history.
     selectSquare(file: string, rank: string): void {
       this.selectedSquare = { file, rank };
       this.history.push({ file, rank });
+
       console.log(`Selected square: ${file}${rank}`);
       console.log(this.history);
     },
