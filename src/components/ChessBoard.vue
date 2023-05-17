1b40a812d8b16d5ed742b648bc3e7b6e1fce6e3a
maximbelo <mb@tineye.com>
2023-05-17 12:40:34 -0400

    Moved out isSelectedSquare logic to a separate func, added pawn and transition

--- src/components/ChessBoard.vue
+++ src/components/ChessBoard.vue
@@ -15,6 +15,11 @@
           {{ rank }}
         </p>
         <!-- Display the files on the far left squares -->
+        <transition name="fade">
+          <div v-if="isSelectedSquare(file, rank)" class="pawn">
+            <img src="/images/pawn.png" alt="A pawn" />
+          </div>
+        </transition>
       </div>
       <!-- Render files (cols) inside the ranks -->
     </div>
@@ -32,23 +37,26 @@ const store = useChessboardStore();
 const files: string[] = ["a", "b", "c", "d", "e", "f", "g", "h"];
 const ranks: string[] = ["1", "2", "3", "4", "5", "6", "7", "8"].reverse();
 
+// When a user clicks on the square, this calls the selectSquare action in the store to update the selected square state
 const selectSquare = (file: string, rank: string): void => {
-  // When a user clicks on the square, this calls the selectSquare action in the Pinia store to update the selected square state
   store.selectSquare(file, rank);
 };
 
+// Check if the square is the selected square
+const isSelectedSquare = (file: string, rank: string): boolean => {
+  return file === store.selectedSquare.file && rank === store.selectedSquare.rank;
+};
+
+// Logic to update square classes
 const getSquareClass = (file: string, rank: string): string => {
   // Determine if the square is a light square based on its position on the board
-  const isDarkSquare = (files.indexOf(file) + 1 + ranks.indexOf(rank)) % 2 === 0;
-
-  // Check if the square is the selected square
-  const isSelectedSquare = file === store.selectedSquare.file && rank === store.selectedSquare.rank;
+  const isDarkSquare: boolean = (files.indexOf(file) + 1 + ranks.indexOf(rank)) % 2 === 0;
 
   // Generate the appropriate CSS class for the square
-  const squareClass = isDarkSquare ? "square dark" : "square light";
+  let squareClass: string = isDarkSquare ? "square dark" : "square light";
 
   // Add the "highlighted" class if the square is selected
-  return isSelectedSquare ? `${squareClass} highlighted` : squareClass;
+  return isSelectedSquare(file, rank) ? `${squareClass} highlighted` : squareClass;
 };
 </script>
 
@@ -100,6 +108,22 @@ const getSquareClass = (file: string, rank: string): string => {
 
   .highlighted {
     background-color: $yellow;
+  }
+
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 0.5s ease;
+  }
+
+  .fade-enter-from,
+  .fade-leave-to {
+    opacity: 0;
+  }
+
+  .pawn img {
+    width: 100%;
+    height: 100%;
+    position: absolute;
   }
 }
 </style>
